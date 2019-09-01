a3833eb5e6c583e1a24b1cb8510b1b0d100509ce
DarioHR <dariokos@mail.com>
2019-09-01 15:58:02 +0200

    Site metadata changed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Dario Blog`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Demo website for the purposes of thesis`,
+    author: `@dariokos`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
